4572b2212960f0af722a953c19d77cf20060cfe2
Abby Sawyer <abbysawyer@blueyonder.co.uk>
2017-03-28 08:40:52 +0100

    Add audio page

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
-angular.module('3LovelyLadiesPage', ['ngRoute', 'RouteControllers', 'NavigationDirective']);
+angular.module('ThreeLovelyLadiesPage', ['ngRoute', 'RouteControllers', 'NavigationDirective']);
 
-angular.module('3LovelyLadiesPage').config(function($locationProvider, $routeProvider){
+angular.module('ThreeLovelyLadiesPage').config(function($locationProvider, $routeProvider){
 	$locationProvider.html5Mode(true);
 
 	$routeProvider.when('/', {
 		templateUrl: 'templates/home.html'
-	});
+	})
+
+	$routeProvider.when('/listen', {
+		templateUrl: 'templates/listen.html'
+	})
+
+	$routeProvider.when('/watch', {
+		templateUrl: 'templates/watch.html'
+	})
+
+	$routeProvider.when('/contact', {
+		templateUrl: 'templates/contact.html'
+	})
 
 });
--- js/directive.js
+++ js/directive.js
@@ -4,4 +4,11 @@ angular.module('NavigationDirective',[])
 			restrict: 'E',
 			templateUrl: 'templates/directives/show-navbar.html'
 		};
-	});
+	})
+
+	.directive('showFooter', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'templates/directives/show-footer.html'
+		};
+	})
