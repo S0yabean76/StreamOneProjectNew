05e86e047cca62fc61238f7ec2f713c8fedda103
Abby Sawyer <abbysawyer@blueyonder.co.uk>
2017-05-06 20:57:34 +0100

    Removed ui-bootstrap references and added watch page content

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-angular.module('ThreeLovelyLadiesPage', ['ngRoute', 'RouteControllers', 'NavigationDirective', 'ui.bootstrap']);
+angular.module('ThreeLovelyLadiesPage', ['ngRoute', 'RouteControllers', 'NavigationDirective']);
 
 angular.module('ThreeLovelyLadiesPage').config(function($locationProvider, $routeProvider){
 	$locationProvider.html5Mode(true);
 
 	$routeProvider.when('/', {
-		templateUrl: 'templates/home.html'
+		templateUrl: 'templates/home.html',
 	})
 
 	$routeProvider.when('/listen', {
--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,2 @@
-angular.module('RouteControllers', [])
-
-	
-	
+angular.module('RouteControllers', ['ui.bootstrap'])
+		
